fbcf8b5b1faa1b904abbe22aa0ceebd154878932
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-05-03 16:07:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ colorama.init()
 def sniper_threaded():
 	choices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 	while True:
-		random1 = random.choice(choices)
-		random2 = random.choice(choices)
-		random3 = random.choice(choices)
-		random4 = random.choice(choices)
-		random5 = random.choice(choices)
-		r = requests.get(f"https://api.roblox.com/users/get-by-username?username={random1}{random2}{random3}{random4}{random5}").text
-		r = str(r)
-		if "User not found" in r:
-			print(colorama.Fore.GREEN + f"Generated Valid Name, {random1}{random2}{random3}{random4}{random5}")
-			if save == "y":
-				file = open("valid_roblox_names.txt", "a")
-				file.write(f"{random1}{random2}{random3}{random4}{random5}\n")
-				file.close()
-		else:
-			print(colorama.Fore.RED + f"Generated Invalid Name, {random1}{random2}{random3}{random4}{random5}")
+		try:
+			random1 = random.choice(choices)
+			random2 = random.choice(choices)
+			random3 = random.choice(choices)
+			random4 = random.choice(choices)
+			random5 = random.choice(choices)
+			r = requests.get(f"https://api.roblox.com/users/get-by-username?username={random1}{random2}{random3}{random4}{random5}").text
+			r = str(r)
+			if "User not found" in r:
+				print(colorama.Fore.GREEN + f"Generated Valid Name, {random1}{random2}{random3}{random4}{random5}")
+				if save == "y":
+					file = open("../../Dropbox/Vault/Codes/Tools/Roblox 5 Letter Username Sniper/valid_roblox_names.txt", "a")
+					file.write(f"{random1}{random2}{random3}{random4}{random5}\n")
+					file.close()
+			else:
+				print(colorama.Fore.RED + f"Generated Invalid Name, {random1}{random2}{random3}{random4}{random5}")
+		except Exception:
+			print(colorama.Fore.RED + "Unkown ERROR")
 		time.sleep(float(delay))
 print("Roblox 5 Letter Username Sniper, Made By blob#0005")
 while True:
